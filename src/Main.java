811cd2882e5defc0be5b3bc787d04bfd487eacb9
Home <ivan.latyshev.2015@yandex.ru>
2025-09-02 20:51:02 +0800

    Added change

--- src/Main.java
+++ src/Main.java
@@ -72,12 +72,13 @@ public class Main {
         int one = 1;
         int two = 2;
         int three = 3;
-        if ((one > two) && (one > three))
-        System.out.println(one);
+        if ((one > two) && (one > three)) {
+            System.out.println(one);
+        }
         else {
-        if ((two > one) && (two > three))
+        if ((two > one) && (two > three)) {
             System.out.println(two);
-        else {
+        } else {
             System.out.println(three);
             }
         }
