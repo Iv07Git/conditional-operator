17a7950c02a082ebbca9f3526f43b656c747d1d3
Home <ivan.latyshev.2015@yandex.ru>
2025-09-03 13:42:41 +0800

    Added change task 7

--- src/Main.java
+++ src/Main.java
@@ -75,12 +75,11 @@ public class Main {
         if ((one > two) && (one > three)) {
             System.out.println(one);
         }
-        else {
         if ((two > one) && (two > three)) {
             System.out.println(two);
-        } else {
+        }
+        if ((three > one) && (three > two)) {
             System.out.println(three);
-            }
         }
 
     }
